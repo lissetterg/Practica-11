82f08cecba4dd19add0c651d780e2d5b02856257
lissetterg <alegnaguacamole@gmail.com>
2024-11-16 23:42:33 -0600

    Create Programa 1.c

---
+++ Programa 1.c
@@ -0,0 +1,20 @@
+/*Reyes Gómez Angela Lissette 
+practica 11 -imprimir inverso*/
+
+#include <stdio.h>
+#include <string.h>
+
+void imprimir(char[]);
+int main ()
+{
+    char nombre[] = "Facultad de Ingenieria";
+    imprimir(nombre);
+}
+
+void imprimir(char s[])
+{
+    int tam;
+    for ( tam=strlen(s)-1 ; tam>=0 ; tam--)
+        printf("%c", s[tam]);
+    printf("\n");
+}
